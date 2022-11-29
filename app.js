96b360918d74e10569ee242fc3be02b26f4edb2e
maurcord <67590886+maurcord@users.noreply.github.com>
2022-11-29 16:27:08 -0500

    test

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { title } = require('process');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+console.log(title)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
